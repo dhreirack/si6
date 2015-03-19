a199c1149d81f0ecf9beb861e775e91e38cfd5c7
Dhrei portable <dhreirack@gmail.com>
2015-03-19 08:26:49 +0100

    si6 github

---
+++ siteadmin/page/users/createuser.php
@@ -0,0 +1,7 @@
+<form action="index.php?controller=user&action=store" method="post">
+	Login : <input type="text" name="login">
+	Mot de passe : <input type="text" name="pwd">
+	Confirmation : <input type="text" name="cpwd">
+
+	<input type="submit">
+</form>
